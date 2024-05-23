69197795796acb027b19dfbd1a1b363f0f0b03b3
Jasmin Hidanovic <dexinho008@gmail.com>
2024-05-23 19:39:11 +0200

    added styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-slate-400 flex justify-center items-center container m-auto p-2`}>{children}</body>
+    <html lang="en" className="dark">
+      <body
+        className={`${inter.className} bg-slate-400 flex justify-center items-center container m-auto p-2 font-black`}
+      >
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
